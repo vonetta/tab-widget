ad93d6a7aac01addf888d03062a17c3a9606b517
Vonetta <vonettastevenson@gmail.com>
2017-11-06 16:55:50 -0800

    little tab-widget in JS

---
+++ JQueryMaster/JavaScript.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+
+    $(".item").on("click", function () {
+        var number = $(this).data("option"); //item i am clicking on
+        console.log(number)
+
+        //remove all actives
+        $(".tab_header .item").removeClass("active");
+
+        //add active
+        $(this).addClass("active");
+
+        $(".tab_container .container_item").hide();
+
+        $("div[data-item=" + number + "]").show();
+
+    })
+
+  
+
+
+
+
+});
