746b344fcdc2cf411feca9daacb7ae3d0d02437c
Vonetta <vonettastevenson@gmail.com>
2017-11-08 15:41:54 -0800

    added card looping

--- JQueryMaster/cardSlider.js
+++ JQueryMaster/cardSlider.js
@@ -1,19 +1,31 @@
 ﻿$(document).ready(function () {
 
+    var itemLength = $('.cards_container item').length;
     var current = 1;
-
-
 
     $('.cardSlider .btn_prev').on("click", function () {
         console.log('go to previous')
-        current = current - 1;
-        showSlider(current);
+
+        if (current > 1){
+            current = current - 1;
+            showSlider(current);
+        } else {
+            current = itemLength;
+            showSlider(current)
+        }
     });
 
     $('.cardSlider .btn_next').on("click", function () {
         console.log("go to next")
-        current = current + 1;
-        showSlider(current);
+
+
+        if (current !== itemLength) {
+            current = current + 1;
+            showSlider(current);
+        } else {
+            current = 1;
+            showSlider(current)
+        }
 
     });
 
