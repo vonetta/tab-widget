a1b41e53d8a64e33254edc7c0a2c1c1045d1827c
Vonetta <vonettastevenson@gmail.com>
2017-11-08 15:30:34 -0800

    added functionality to buttons

--- JQueryMaster/cardSlider.js
+++ JQueryMaster/cardSlider.js
@@ -1,1 +1,25 @@
-﻿
+﻿$(document).ready(function () {
+
+    var current = 1;
+
+
+
+    $('.cardSlider .btn_prev').on("click", function () {
+        console.log('go to previous')
+        current = current - 1;
+        showSlider(current);
+    });
+
+    $('.cardSlider .btn_next').on("click", function () {
+        console.log("go to next")
+        current = current + 1;
+        showSlider(current);
+
+    });
+
+    function showSlider(current) {
+        $(".cards_container .item").removeClass("active");
+        $("div[data-id='" + current + "']").addClass("active");
+    }
+
+});
