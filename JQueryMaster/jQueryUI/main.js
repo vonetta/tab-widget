24526512470cd84fc87730e0d37aa83fc5c37d66
Vonetta <vonettastevenson@gmail.com>
2017-11-08 23:58:51 -0800

    some jquery UI stuff

---
+++ JQueryMaster/jQueryUI/main.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+
+    $(document).click(function () {
+        //$(".button").toggle("drop", 1000, function () {
+        //    console.log("done")
+        //})
+
+        //$(".button").toggleClass("otherClass", 1000)
+
+        $(".button").switchClass("button", "otherClass",1000)
+    })
+})
