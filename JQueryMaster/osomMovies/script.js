ee0b226d9660c7b4ea64657b4cedab7d54353cee
Vonetta <vonettastevenson@gmail.com>
2017-11-11 00:59:11 -0800

    added basic functionality to the filter button

--- JQueryMaster/osomMovies/script.js
+++ JQueryMaster/osomMovies/script.js
@@ -1,4 +1,25 @@
-﻿$(document).ready(function () {
+﻿var osomMovie = {};
 
+osomMovie.init = function () {
+    osomMovie.filterSlider();
+};
 
-})
+osomMovie.filterSlider = function () {
+    $(".filter.open").on("click", function () {
+
+        $(".filter_container").slideToggle(300, function () {
+            var btn = $(this).prev();
+
+            if (btn.hasClass("active")) {
+                $("filter.open").find(".btn_title").text("Filter by");
+                btn.removeClass("active");
+            }
+            else {
+                $("filter.open").find(".btn_title").text("close");
+                btn.addClass("active");
+            }
+        });
+    })
+};
+
+osomMovie.init();
